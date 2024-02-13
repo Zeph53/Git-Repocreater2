ecc894579e0585b78c2cd772071e3564db1c68e9
BuildTools <unconfigured@null.spigotmc.org>
2024-02-13 08:53:53 -0500

    commit

--- git-repocreater2.bash
+++ git-repocreater2.bash
@@ -506,44 +506,31 @@ fi
 
 #
 ## Creating a description for your repository
-while [[ -z "$confirm_edited_description" || "$confirm_edited_description" == "false" ]]; do
+while [[ -z "$confirm_edited_description" || "$confirm_edited_description" != "true" ]]; do
   if [[ "$git_repo_created" == "true" || "$git_repo_exists" == "true" ]]; then
     current_description="$(gh repo view "$git_username/$repo_name" --json "description" | awk -F '"' '{print $4}')"
-    while [[ -z "$description_exceeds_limit" || "$description_exceeds_limit" == "true" ]]; do
-      printf "Edit the description for \"$git_repo_url\". 350 characters max.\n"
-      if [[ -z "$current_description" ]]; then
-        current_description="No description, website, or topics provided."
-      fi
-      if [[ "$description_exceeds_limit" == "true" ]]; then
-        current_description="$edited_description"
-      fi
-      read -r -e -i "$current_description" "edited_description"
-      if (( "${#edited_description}" >= 0 && "${#edited_description}" <= 350 )); then
-        printf "Description saved.\n"
-        description_saved="true"
-        description_exceeds_limit="false"
-        break 1
-      else
-        printf "Description exceeds the 350 character limit.\n"
-        description_exceeds_limit="true"
-      fi
-    done
-  fi
-  # Confirm correct description
-  if [[ "$description_saved" == "true" ]]; then
-    printf "Description: \"$edited_description\"\n"
-    while [[ -z "$confirm_edited_description" ]]; do
-      printf "Is this correct? Yes/No: "
-      read -r "confirm_edited_description"
-      confirm_edited_description="$(printf "$confirm_edited_description" | tr '[:upper:]' '[:lower:]')"
-      if [[ "$confirm_edited_description" == "yes" || "$confirm_edited_description" == "y" ]]; then
-        confirm_edited_description="true"
-        break 1
-      elif [[ "$confirm_edited_description" == "no" || "$confirm_edited_description" == "n" ]]; then
-        description_saved="false"
-        confirm_edited_description="false"
-      fi
-    done
+    printf "Edit the description for \"$git_repo_url\". 350 characters max.\n"
+    if [[ -z "$current_description" ]]; then
+      current_description="No description, website, or topics provided."
+    fi
+    read -r -e -i "$current_description" edited_description
+    if (( ${#edited_description} >= 0 && ${#edited_description} <= 350 )); then
+      printf "Description saved.\n"
+      printf "Description: \"$edited_description\"\n"
+      while [[ -z "$confirm_edited_description" ]]; do
+        printf "Is this correct? Yes/No: "
+        read -r confirm_edited_description
+        confirm_edited_description="$(printf "%s" "$confirm_edited_description" | tr '[:upper:]' '[:lower:]')"
+        if [[ "$confirm_edited_description" == "yes" || "$confirm_edited_description" == "y" ]]; then
+          confirm_edited_description="true"
+        elif [[ "$confirm_edited_description" == "no" || "$confirm_edited_description" == "n" ]]; then
+          confirm_edited_description="false"
+          break
+        fi
+      done
+    else
+      printf "Description exceeds the 350 character limit.\n"
+    fi
   fi
 done
 
